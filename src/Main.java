ca30d087c22c735d07de6f53b0a8c868b680fd69
xinxin-zhang <35882932+xinxin-zhang@users.noreply.github.com>
2018-02-16 03:12:56 -0600

    Delete Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-import java.io.*;
-public class Main {
-    public static void main(String[] args) throws IOException {
-        donation_analytics don = new donation_analytics();
-        don.run();
-    }
-}
