dbb232c06ff294025b4dac58c8bd096a199441cb
xinxin-zhang <35882932+xinxin-zhang@users.noreply.github.com>
2018-02-16 17:11:42 +0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        donation_analytics don = new donation_analytics();
+        don.run();
+    }
+}
